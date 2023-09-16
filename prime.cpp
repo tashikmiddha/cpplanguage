1bb1803f17c4b06bb10ffe76eb405fb4831752eb
tashikmiddha <tashikmiddha@gmail.com>
2023-09-17 00:10:57 +0530

    program to find the number is prime or not

---
+++ prime.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    for (int i = 2; i < sqrt(n); i++)
+    {
+       if(n%i==0)
+       {
+        cout<<"not a prime number"<<endl;
+       return 0;
+       }
+    }
+    cout<<"prime number"<<endl;
+    return 0;
+    
+}

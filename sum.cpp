aee1ba21ffa27a0a3adb92ae88425521484a271e
tashikmiddha <tashikmiddha@gmail.com>
2023-09-16 23:21:48 +0530

    sum program is added

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers "<<endl;// this is a pre defined function in iostream header file which is used to print something
+    cin>>a>>b;// this is used to input something from the user 
+    cout<<"the sum is "<<a+b;
+    return 0;
+}

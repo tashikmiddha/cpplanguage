a7f1a4d45fdb03ef65870928207b8050ab28915e
tashikmiddha <tashikmiddha@gmail.com>
2023-09-23 23:55:03 +0530

    palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the size of character array"<<endl;
+    cin>>n;
+    char arr[n+1];
+    cout<<"enter the character array"<<endl;
+    cin>>arr;
+    int check =1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]!=arr[n-1-i])
+        {
+            check =0;
+            break;
+        }
+    }
+    if(check==true)
+    {
+     cout<<"word is a palindrome"<<endl;
+    }
+    else
+    {
+        cout<<"word is not a palindrome"<<endl;
+    }
+ return 0;
+}

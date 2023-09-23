3e3f87bb59452208565ee605c35999d4e92b28e9
tashikmiddha <tashikmiddha@gmail.com>
2023-09-23 23:51:10 +0530

    first repeating element

---
+++ firstrepeatingelement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the size of the array"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements of the array"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[n];
+    }
+ return 0;
+}

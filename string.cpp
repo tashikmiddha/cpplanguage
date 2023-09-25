cbb528e67ffcf1c1e8ea0c0f7d46ca437a6f8280
tashikmiddha <tashikmiddha@gmail.com>
2023-09-25 22:21:02 +0530

    string

---
+++ string.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    //int n=100;
+    //char arr[n];
+    string s,d="7462846272637";
+    //getline(cin,s);
+    // cout<<s;
+    // cin.getline(arr,n);
+     //d.erase(7,6);
+     //cout<<d;
+     sort(d.begin(),d.end());
+     cout<<d<<endl;
+     system("pause");
+    return 0;
+
+}

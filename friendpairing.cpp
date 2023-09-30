d1a762ddc9f6a44b10fa1b1875b0b9714beefda1
tashikmiddha <tashikmiddha@gmail.com>
2023-09-30 21:05:51 +0530

    friend pairing

---
+++ friendpairing.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int pairing(int n)
+{
+    if(n==0||n==1||n==2)
+    {
+        return n;
+    }
+    return pairing(n-1) +pairing(n-2)*(n-1);
+}
+int main()
+{
+    cout<<pairing(4);
+ return 0;
+}

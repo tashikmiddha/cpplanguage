e2c551779f24c4b8b25acabe6c3a3cdb827ee7f4
tashikmiddha <tashikmiddha@gmail.com>
2023-10-02 23:14:52 +0530

    pause system

---
+++ pauseasystem.cpp
@@ -0,0 +1,14 @@
+
+// C++ program to pause program 
+// using pause() method
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
+  
+// Driver code
+int main() 
+{
+   cout << "Welcome To Geeksforgeek "<<endl;
+   system("pause");
+   cout << "Done";
+}
